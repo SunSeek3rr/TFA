25cd694980f83637bab081e985edde029a07b8e2
SunSeek3rr <sebastien.dagostin@student.heaj.be>
2025-06-03 16:55:43 +0200

    portfolio + correction meta DF
    HTML/CSS pour la page principale, mobile et tablette + menu JS
    meta partage et clamp principaux
    
    reste :
    Page Case study*4, Page Crédits en HTML/CSS

--- Portfolio/src/scripts/app.js
+++ Portfolio/src/scripts/app.js
@@ -1,1 +1,13 @@
-console.info('Hello world');
+// Header nav
+let menuBtn = document.querySelector(".menu__btn");
+menuBtn.addEventListener("click", ()=>{
+    let menu = document.querySelector(".menu");
+    let menuLinks = document.querySelectorAll(".menu__el--link");
+    menu.classList.toggle("menu--open");
+    menuLinks.forEach((item)=>{
+        item.addEventListener("click", ()=>{
+            menu.classList.remove("menu--open");
+        });
+    });
+
+});
