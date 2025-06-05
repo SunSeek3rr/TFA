bb9746cfa88af1e782485723e957dc8193542eb8
SunSeek3rr <sebastien.dagostin@student.heaj.be>
2025-06-06 00:47:49 +0200

    Portfolio fini
    Case study*4 fini, corrections design et fonctionnement au niveau de l'affichage des cases study (main disparait et case study s'affiche).
    
    Section__btn remplacés par des liens
    
    Validé W3C
    
    reste :
    page crédits
    compresser les images

--- Portfolio/src/scripts/app.js
+++ Portfolio/src/scripts/app.js
@@ -1,6 +1,5 @@
-
-
 // Header nav
+let headerNav = document.querySelector(".header");
 let menuBtn = document.querySelector(".menu__btn");
 menuBtn.addEventListener("click", ()=>{
     let menu = document.querySelector(".menu");
@@ -13,3 +12,54 @@ menuBtn.addEventListener("click", ()=>{
     });
 
 });
+//Case study open btn
+
+
+let projetsBtn = document.querySelectorAll(".projets__btn");
+let main = document.querySelector(".main");
+projetsBtn.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        if(item.classList.contains("projets__btn--janus")){
+            let janusCase = document.querySelector(".caseStudy--janus");
+            janusCase.classList.add("open");
+        } else if (item.classList.contains("projets__btn--dataplay")){
+            let dataplayCase = document.querySelector(".caseStudy--dataplay");
+            dataplayCase.classList.add("open");
+        } else if(item.classList.contains("projets__btn--rux")){
+            let ruxCase = document.querySelector(".caseStudy--rux");
+            ruxCase.classList.add("open");
+        }else if(item.classList.contains("projets__btn--df")){
+            let dfCase = document.querySelector(".caseStudy--df");
+            dfCase.classList.add("open");
+        }
+        headerNav.style.display = "none";
+        main.style.display= "none";
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
+
+let projetsBtnInside = document.querySelectorAll(".section--caseStudy .projets__btn");
+projetsBtnInside.forEach((item)=>{
+    item.addEventListener("click",()=>{
+        let activeCase = document.querySelector(".open");
+        activeCase.classList.remove("open");
+    });
+});
+
+// Case study close btn
+
+let closeBtn = document.querySelectorAll(".header__btn");
+
+closeBtn.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        let activeCase = document.querySelector(".open");
+        activeCase.classList.remove("open");
+        headerNav.style.display = "block";
+        main.style.display= "block";
+        let projetsID = document.getElementById("Projets");
+        window.scrollTo({ top: projetsID.offsetTop - 60, behavior: "smooth" });
+
+    });
+});
+
+
